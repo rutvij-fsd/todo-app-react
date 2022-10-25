f414c446eeb5e0ba978c547fbe7135ac147a71d8
rutvij-fsd <rutvijpatel414@gmail.com>
2022-10-25 16:45:55 +0530

    Add todoArr state onTodoAddHandler

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
-
+import { v4 as uuidv4 } from "uuid";
 const Todo = () => {
   const [input, setInput] = useState("");
-  console.log('input', input);
+  const [todoArr, setTodoArr] = useState([]);
 
   const onTodoAddHandler = () => {
-    console.log('add Todo');
+    if (!input) return;
+    setTodoArr([
+      ...todoArr,
+      {
+        id: uuidv4().split("-")[0],
+        text: input
+      }
+    ]);
+    
+    setInput("");
   };
+  
   return (
     <div className="container text-center">
       <h1 className="m-5">Todo App</h1>
@@ -24,6 +34,15 @@ const Todo = () => {
         >
           Add
         </button>
+      </div>
+      <div className="container">
+        {todoArr.map((todo, index) => (
+          <ul style={{ listStyle: "none" }} className="row mt-3">
+            <li className="col-6 text-center fs-4 ms-5">{todo.text}</li>
+            <button className="col-auto btn btn-secondary me-2">Edit</button>
+            <button className="col-auto btn btn-danger h-75">Delete</button>
+          </ul>
+        ))}
       </div>
     </div>
   );
